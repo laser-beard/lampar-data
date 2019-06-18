562dc4357f784c867c04d7449c2621f792739ac0
therealmolotov <molotov.vn@gmail.com>
2019-06-19 00:15:27 +0300

    some logic

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ if sys.version_info[0] < 3:
         raise RuntimeError('Must be run using python 3.x')
 
 
-locale.setlocale(locale.LC_ALL, "de_DE.utf8")
+locale.setlocale(locale.LC_ALL, "ru_RU.utf8")
 
 current_locale = locale.getlocale()[0][:2]
 
 print(current_locale)
 
-date = '7 mär, 2019'
+date = '17 мар, 2019'
 
+# parsing DAY
 a = date.split(' ')
 day = a[0]
+if day[0] != '0' and len(day) == 1:
+    day = '0' + day
+print(day)
+
+# parsing MONTH
 month = a[1][:3].lower()
+
+
 year = a[2]
+
+
 print(day, month, year)
 
 # dates = ['05.12.1989', '8 jan 2019']
